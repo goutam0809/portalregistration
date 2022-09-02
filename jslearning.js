474bd5627450de8190b382bf9aca0ab4ca9291c7
goutam0809 <singh.gautam0809@gmail.com>
2022-09-03 00:47:33 +0530

    jslearning

---
+++ jslearning.js
@@ -0,0 +1,50 @@
+////////////////Const, blocked scope, varialable declaration////////////////
+// // const a = 10;
+// // {
+// //   const funtion1 = 12;
+// //   const a = 15;
+
+// //   //console.log(a);
+// // }
+// // console.log(a);
+
+////////////////function scope, parametrized function, function trigger////////////////
+// const factorial=()=> {
+// //   var fact = 1;
+// //   if (x == 0) {
+// //     return 1;
+// //   } else {
+// //     for (var i = 1; i <= x; i++) {
+// //       fact = fact * i;
+// //       console.log("!" + i + " = " + fact);
+// //     }
+// //   }
+// return "Goutam";
+// }
+// // factorial(10);
+// console.log (factorial());
+/*anything
+
+///////Types of comment/////////////
+//Goutam// (//single line comment)
+Srishti*/ ////(Multiline comment)/////
+/**descriptive comments
+ * "Here you can document the details"
+*/
+
+/////////Variable declaration using Let////////////
+// Variables defined with let cannot be Redeclared.
+// Variables defined with let must be Declared before use.
+// Variables defined with let have Block Scope.
+// {
+// let a =10;
+//     a =20;
+//     console.log(a);
+// }
+// // Question: what is the outcome of below?//
+// let a=10;
+// let a=b=20;
+// console.log(a);
+// console.log(b); ////////Answer is "error" as its going against from 1st statement////////
+
+
